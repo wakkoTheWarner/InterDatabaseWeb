5dd50466c621459f5add227ef463360f1ba9d9ff
Eddy Sosa <eduardo-elrey-@hotmail.com>
2024-04-30 16:58:06 -0400

    Version: 3.0, Date: 2024-04-24, Build: 2 -- Implement external config file and increase session security
    The commit introduces an external configuration file for the database and enhances the session security features. The configuration file now handles database connection details which can be modified easily. Additional security measures have been added for session cookies, and user inputs are now sanitized before processing.

--- templates/adminPages/adminDashboard.php
+++ templates/adminPages/adminDashboard.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 if (!isset($_SESSION['email'])) {
@@ -12,10 +13,31 @@ if (!isset($_SESSION['email'])) {
 <head>
     <meta charset="UTF-8">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../../static/css/adminDashboard.css">
 </head>
 <body>
+    <div id="headerNav">
+        <div class="logo">
+            <a href="#">
+                <img src="../../static/img/inter-logo-full.png" alt="Inter CurricuLab">
+            </a>
+        </div>
+        <nav>
+            <a href="#">Dashboard</a>
+            <a href="#">Terms</a>
+            <a href="#">Programs</a>
+            <a href="#">Courses</a>
+        </nav>
+        <div class="userBox">
+            <button onclick="myFunction()" class="userDropdownButton">Welcome, <?php echo $_SESSION['email']; ?></button>
+            <div id="userDropdown" class="dropdownContent">
+                <a href="#">Profile</a>
+                <a href="#">Settings</a>
+                <a href="#" style="background-color: #FF9999;">Logout</a>
+            </div>
+        </div>
+    </div>
     <div id="container">
-        <h1>HI!</h1>
         <button id="logout">Log Out</button>
     </div>
     <script>
@@ -28,6 +50,23 @@ if (!isset($_SESSION['email'])) {
                     }
                 });
         });
+
+        function myFunction() {
+            document.getElementById("userDropdown").classList.toggle("show");
+        }
+
+        window.onclick = function(event) {
+            if (!event.target.matches('.userDropdownButton')) {
+                var dropdowns = document.getElementsByClassName("dropdownContent");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
     </script>
 </body>
 </html>
