ef8001434307cd50d004f91a32d3095b1c59de94
Eddy Sosa <eduardo-elrey-@hotmail.com>
2024-05-07 16:49:14 -0400

    Version: 3.0, Date: 2024-05-07, Build: 25 -- Add adminProgramCourses page with associated JS and CSS
    A new page named adminProgramCourses.php has been created for the admin dashboard. Corresponding JavaScript and CSS files for this page, adminProgramCourses.js and adminProgramCourses.css respectively, were also added for interactivity and styling.

---
+++ templates/adminPages/adminProgramCourses.php
@@ -0,0 +1,60 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Program/Courses</title>
+    <link rel="stylesheet" href="../../static/css/headerFooter.css">
+    <link rel="stylesheet" href="../../static/css/adminProgramCourses.css">
+</head>
+<body>
+    <div id="headerNav">
+        <div class="logo">
+            <a href="adminDashboard.php">
+                <img src="../../static/img/inter-logo-full.png" alt="Inter CurricuLab">
+            </a>
+        </div>
+        <nav>
+            <a href="adminDashboard.php">Dashboard</a>
+            <a href="adminTerms.php">Terms</a>
+            <a href="#">Programs</a>
+            <div class="dropdown">
+                <a href="adminCourses.php">Courses</a>
+                <div class="dropdownContent">
+                    <a href="adminCompetency.php">Competencies</a>
+                    <a href="adminSection.php">Section</a>
+                </div>
+            </div>
+        </nav>
+        <div class="userBox">
+            <button onclick="myFunction()" class="userDropdownButton">
+                <img src="../../static/img/userProfile.jpg" alt="User" >
+                <?php
+                echo $_SESSION['firstName'] . ' ' . $_SESSION['lastName'];
+                ?>
+            </button>
+            <div id="userDropdown" class="dropdownContent">
+                <a href="#">Profile</a>
+                <?php
+                if ($_SESSION['accountType'] === 'Admin' || $_SESSION['accountType'] === 'Root') {
+                    echo '<a href="adminLogger.php">Logger</a>';
+                }
+                ?>
+                <a href="adminUsers.php">Users</a>
+                <a id="logout">Log Out</a>
+            </div>
+        </div>
+    </div>
+    <div id="container">
+        <div class="container-upperBox">
+
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2024 Inter CurricuLab</p>
+    </footer>
+    <script src="../../static/js/adminProgramCourses.js"></script>
+</body>
+</html>
