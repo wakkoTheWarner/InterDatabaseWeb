5da019dfb8438384d6bd486f39b519a2da384209
Eddy Sosa <eduardo-elrey-@hotmail.com>
2024-05-08 19:20:12 -0400

    Version: 3.0, Date: 2024-05-08, Build: 33 -- Added admin term sections management page
    Created new admin term sections management page with respective HTML structure, styles, and JavaScript. The page provides an interface for admins to manage term sections and includes error handling, session management, user privilege checks, database connection, and logout functionality.

---
+++ static/js/adminTermSections.js
@@ -0,0 +1,28 @@
+// -- Logout Function --
+document.getElementById('logout').addEventListener('click', function() {
+    fetch('../../backend/php/logout.php')
+        .then(response => response.text())
+        .then(data => {
+            if(data === 'success') {
+                window.location.href = '../index.php';
+            }
+        });
+});
+
+// -- UserBox Dropdown --
+function myFunction() {
+    document.getElementById("userDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.userDropdownButton')) {
+        var dropdowns = document.getElementsByClassName("dropdownContent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
